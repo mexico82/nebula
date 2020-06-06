a59964509a5805006d701d7c4d29314acba59fd8
hersel91 <hersel1991@gmail.com>
2020-06-06 21:13:57 +0200

    general fix

--- core/modules/commands/owner/test.py
+++ core/modules/commands/owner/test.py
@@ -3,4 +3,4 @@ import core.decorators
 @core.decorators.owner.init
 def init(update,context):
     bot = context.bot
-    bot.send_message(update.message.chat_id, text="OWNER COMMAND")
+    bot.send_message(update.message.chat_id, text="OWNER COMMAND TEST")
